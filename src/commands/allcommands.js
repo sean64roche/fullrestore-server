8213e5ad2ebbb5905da9330313222c0277eb366d
Sean Roche <sean64roche@gmail.com>
2024-12-15 15:14:40 +0000

    remove duplicate pool command

--- src/commands/allcommands.js
+++ src/commands/allcommands.js
@@ -1,5 +1,4 @@
-import { POOL_COMMAND } from './utilities/pool.js';
 import { ROUND_COMMAND } from './utilities/round.js';
 import { TEST_COMMAND } from './utilities/test.js';
 
-export default [POOL_COMMAND, TEST_COMMAND, ROUND_COMMAND];
+export default [TEST_COMMAND, ROUND_COMMAND];
