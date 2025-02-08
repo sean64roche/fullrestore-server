ad1a19c4d19abe24dad5dc5e9275d9004d022d94
Sean Roche <sean64roche@gmail.com>
2025-02-08 14:40:44 +0000

    export repositories

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import {ApiConfig} from "./config";
+import PlayerRepository from "./repositories/playerRepository";
+import TournamentRepository from "./repositories/tournamentRepository";
+import RoundRepository from "./repositories/roundRepository";
+import FormatRepository from "./repositories/formatRepository";
+import RoundByeRepository from "./repositories/roundByeRepository";
+import PairingRepository from "./repositories/pairingRepository";
+export default {
+    PlayerRepository,
+    TournamentRepository,
+    RoundRepository,
+    FormatRepository,
+    RoundByeRepository,
+    PairingRepository,
+};
 
 export { PairingImportService } from './services/pairingImportService';
 export { PlayerImportService } from "./services/playerImportService";
@@ -10,7 +24,6 @@ export type { SheetData } from './interfaces/import';
 export { SheetPairing, PairingDto, Pairing, ReplayDto, Replay, SheetBye, RoundByeDto, RoundBye } from './interfaces/pairing';
 export { SheetPlayer, PlayerDto, Player, PlayerAlias, EntrantPlayer, EntrantPlayerDto } from './interfaces/player';
 export { SheetTournament, TournamentDto, Tournament, SheetRound, RoundDto, Round } from './interfaces/tournament';
-
 export const createConfig = (overrides?: Partial<ApiConfig>): ApiConfig => {
     return {
         ...DEFAULT_CONFIG,
