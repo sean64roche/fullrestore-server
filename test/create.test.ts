4eff09c326e7a4cb979214bc8cbe0833ef41179d
Sean Roche <sean64roche@gmail.com>
2025-03-16 12:12:49 +0000

    player username formatting tests, not passing yet

--- test/create.test.ts
+++ test/create.test.ts
@@ -29,6 +29,12 @@ test.describe('POST /api/players', () => {
         discord_user: 'testdiscord1',
         discord_id: '01234567890',
     };
+
+    const psUser2 = 'Cool Gamer 23 ;) 万千白';
+    const discordUser2 = 'Cool_Gamer_23';
+    const psUser2Transformed = 'coolgamer23';
+    const discordUser2Transformed = 'cool_gamer_23';
+
     test('POST /api/players new player succeeds', { timeout: 10000 }, async () => {
         const response = await request(app).post('/api/players')
             .send(body)
@@ -36,24 +42,34 @@ test.describe('POST /api/players', () => {
             .set('Content-Type', 'application/json');
         assert.equal(response.status, 201);
         assert.ok(response.body.id);
-        assert.equal(response.body.ps_user, 'testplayer1');
+        assert.equal(response.body.ps_user, body.ps_user);
         assert.equal(response.body.discord_user, 'testdiscord1');
         assert.equal(response.body.discord_id, '01234567890');
     });
 
-    test('POST /api/players also creates alias entry', { timeout: 10000 }, async () => {
-        const response = await request(app).get(`/api/players?player=${body.ps_user}`);
+    test('POST /api/players transforms PS usernames and discord usernames correctly', { timeout: 10000 }, async () => {
+        const response = await request(app).post('/api/players')
+            .send({ ps_user: psUser2, discord_user: discordUser2 })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json');
+        assert.equal(response.status, 201);
+        assert.equal(response.body.ps_user, psUser2Transformed);
+        assert.equal(response.body.discord_user, discordUser2Transformed);
+    });
+
+    test('POST /api/players also creates alias entry correctly', { timeout: 10000 }, async () => {
+        const response = await request(app).get(`/api/players?player=${psUser2}`);
         assert.equal(response.status, 200);
         assert.ok(response.body.Aliases.some(
             (alias: {
                 ps_alias: string;
-            }) => alias.ps_alias === body.ps_user
+            }) => alias.ps_alias === psUser2Transformed
         ));
     });
 
-    test ('POST /api/players duplicate player fails', { timeout: 10000 }, async () => {
+    test('POST /api/players duplicate player fails', { timeout: 10000 }, async () => {
         const response = await request(app).post('/api/players/')
-            .send({ ps_user: 'rezzo64' })
+            .send({ ps_user: psUser2 })
             .set('Accept', 'application/json')
             .set('Content-Type', 'application/json');
         assert.equal(response.status, 409);
