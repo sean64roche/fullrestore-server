4abb3ec78f5e45bde16a25adeb2e647da44cc048
Sean Roche <sean64roche@gmail.com>
2025-05-27 19:16:54 +0100

    use a proper filepath

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -72,14 +72,14 @@ log4js.configure({
         console: { type: 'console' },
         app: {
             type: 'file',
-            filename: '/app/logs/app.log',
+            filename: '/logs/app.log',
             maxLogSize: 10485760, // 10MB
             backups: 5,
             compress: true
         },
         error: {
             type: 'file',
-            filename: '/app/logs/error.log',
+            filename: '/logs/error.log',
             maxLogSize: 10485760,
             backups: 5,
             compress: true
