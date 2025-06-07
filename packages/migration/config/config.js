08d7a168c8982f8410a32586f960386f3f9ce956
Sean Roche <sean64roche@gmail.com>
2025-06-07 19:14:36 +0100

    merge deployment strategies

--- packages/migration/config/config.js
+++ packages/migration/config/config.js
@@ -5,21 +5,21 @@ module.exports = {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
 
   }
