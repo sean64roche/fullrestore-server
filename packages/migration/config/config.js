77f56ea0ca47ffaeb62e05755311823785576ff3
Sean Roche <sean64roche@gmail.com>
2025-06-08 11:12:12 +0100

    normalise env var names

--- packages/migration/config/config.js
+++ packages/migration/config/config.js
@@ -2,23 +2,23 @@ require("dotenv").config();
 
 module.exports = {
   "development": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
 
